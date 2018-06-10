7811aa67c52ba5aea04b6bb96da606b255a0005e
kal93 <maaz2dt@gmail.com>
2018-06-11 02:32:58 +0530

    build & aot changes.

--- src/app/tab2/tab2.component.ts
+++ src/app/tab2/tab2.component.ts
@@ -1,3 +1,4 @@
+import { browser } from 'protractor';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,6 +10,8 @@ export class Tab2Component implements OnInit {
 
   today = Date.now();
   a  = '420.024';
+
+  browserLocale = window.navigator.language;
 
   constructor() { }
 
